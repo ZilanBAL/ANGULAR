92dda446b5ea6db6a89fe30662523d3ad796a697
Zilo <zilanbal@yahoo.fr>
2026-03-10 14:47:10 +0100

    NEW: add not found routine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from '../pages/home/home.component';
+import { NotFound } from '../pages/not_found/not-found.component';
 
 // TODO (Seance 4 - routing): declarer ici les routes lazy-loaded vers
 // Accueil, DetailFilm, Favoris et NotFound.
@@ -9,5 +10,10 @@ export const routes: Routes = [
     path: '',
     component: Home,
     title: 'WishFlix - Acceuil',
+  },
+  {
+    path: '**',
+    component: NotFound,
+    title: '404 - Tu es perdu ?',
   },
 ];
